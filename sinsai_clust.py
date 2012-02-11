46421a6e3bf6f9798ca2b677e1b0b9073ab3977b
cypher512 <saeki512000gmail.com>
2012-02-11 20:02:33 +0900

    no comment

--- sinsai_clust.py
+++ sinsai_clust.py
@@ -34,18 +34,9 @@ html_body = u"""
 </html>
 """
 
-#google analytics用
+#google analytics用(略)
 
 tracking_code=u"""<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-24404952-3']);
-  _gaq.push(['_trackPageview']);
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
 </script>"""
 
 css_code=u"""
